17bab266bae1965824e96d807b800a75bb557099
EvgLav <www.coldreks@gmail.com>
2024-11-28 19:47:52 +0300

    add final task

---
+++ Practice/src/main/java/sprint_1/finalTask/CarProperties.java
@@ -0,0 +1,14 @@
+package sprint_1.finalTask;
+
+class CarProperties {
+    String maxSpeed; // максимальная скорость
+    byte initialScore; // начальное количество очков
+    double acceleration; // ускорение
+    int nitroLevel; // уровень закиси азота
+
+    public CarProperties(String maxSpeed, float acceleration, int nitroLevel) {
+        this.maxSpeed = maxSpeed;
+        this.acceleration = acceleration;
+        this.nitroLevel = nitroLevel;
+    }
+}
